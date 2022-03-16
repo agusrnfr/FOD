4a22c54c3ce32a13660fe778a053db467e32960d
agusrnfr <agustina.rojas193292@alumnos.info.unlp.edu.ar>
2022-03-16 11:45:45 -0300

    PRACTICA 1

--- PRACTICAS/Ejercicio_5/Ejercicio.pas
+++ PRACTICAS/Ejercicio_5/Ejercicio.pas
@@ -32,6 +32,16 @@ archivo = file of celu;
 
 //INGRESAR NOMBRE DE ARCHIVO
 
+procedure imprimir (c:celu);
+begin
+	with c do begin
+		writeln ('|CODIGO: ',cod,' |PRECIO: ',precio:0:2,' |MARCA: ',marca);
+		writeln ('|STOCK DISPONIBLE: ',stockD,' |STOCK MINIMO: ',stockM);
+		writeln ('|DESCRIPCION:',des,' |NOMBRE: ',nombre);
+		writeln ('');
+	end;
+end;
+
 procedure ingresarNombre (var nombre:string);
 begin
 	write ('INGRESE NOMBRE DEL ARCHIVO: '); readln (nombre);
@@ -58,9 +68,63 @@ begin
 	close (arc_log);
 end;
 
-procedure menu (var arc_log:archivo; var carga:Text; nombre:string);
+procedure menosStockMinimo (var arc_log:archivo);
+var
+	c:celu;
+begin
+	reset (arc_log);
+	while not eof (arc_log) do begin
+		read (arc_log,c);
+		if (c.stockD < c.stockM) then begin
+			imprimir (c);
+		end;
+	end;
+	close (arc_log);
+end;
+
+procedure buscarCadena (var arc_log:archivo; cadena:string);
+var
+	c:celu;
+	ok:boolean;
+begin
+	cadena:= ' ' + cadena; //CUANDO LEO LA CADENA DEL TXT LE AGREGA UN ESPACIO AL INICIO.
+	ok:= false;
+	reset (arc_log);
+	while not eof (arc_log) do begin
+		read (arc_log,c);
+		if (c.des = cadena) then begin
+			imprimir (c);
+			ok := true;
+		end;
+	end;
+	if (ok = false) then begin
+	writeln ('NINGUN CELULAR TIENE ESA DESCRIPCION'); writeln ('');
+	end;
+	close (arc_log);
+end;
+
+procedure exportar (var arc_log:archivo; var celu2:Text);
+var
+	c:celu;
+begin
+	rewrite (celu2);
+	reset (arc_log);
+	while not eof (arc_log) do begin
+		read (arc_log,c);
+		with c do begin
+		writeln (celu2,'|CODIGO: ',cod:10,' |PRECIO: ',precio:10:2,' |MARCA: ',marca:10,'|STOCK DISPONIBLE: ',stockD:10,' |STOCK MINIMO: ',stockM:10,
+		'|DESCRIPCION:',des:10,' |NOMBRE: ',nombre:10);
+		end;
+	end;
+	writeln ('SE EXPORTO CON EXITO');
+	close (arc_log);
+	close (celu2);
+end;
+
+procedure menu (var arc_log:archivo; var carga,celu2:Text; nombre:string);
 var
 	opcion:integer;
+	cadena:string;
 begin
 	writeln('INGRESE POR TECLADO LA OPCION QUE DESEA REALIZAR CON EL ARCHIVO ',nombre);
 	writeln ('');
@@ -69,21 +133,32 @@ begin
 	writeln ('2) MOSTRAR EN PANTALLA LOS DATOS DE LOS CELULARES CON STOCK MENOR AL STOCK MINIMO');
 	writeln ('');
 	writeln ('3) MOSTRAR EN PANTALLA CELULARES CUYA DESCRIPCION TENGA UNA CADENAS DE CARACTERES PROPORCIONADA POR EL USUARIO');
+	writeln ('');
+	writeln ('4) EXPORTAR ARCHIVO A "celulares2.txt"');
+	writeln ('');
 	write 	('OPCION ELEGIDA -->  ');
 	readln (opcion);
 	writeln ('');
 	case opcion of
 		1: 
-		begin
 			crear (arc_log,carga);
-		end
+		2:
+			menosStockMinimo (arc_log);
+		3: 
+		begin
+			write ('INGRESE DESCRIPCION DE CELULAR BUSCAR: ');
+			readln(cadena);
+			writeln ('');
+			buscarCadena(arc_log,cadena);
+		end;
+		4:	exportar (arc_log,celu2);
 		else writeln ('NO SE ENCUENTRA ESA OPCION');	
 	end;
 end;
 
 var
 	arc_log: archivo;
-	carga: Text;
+	carga,celu2: Text;
 	nombre:string;
 	letra:char;
 	loop:boolean;
@@ -95,14 +170,16 @@ begin
 	ingresarNombre (nombre);
 	Assign (arc_log,nombre);
 	Assign (carga,'celulares.txt');
-	menu (arc_log,carga,nombre);
+	Assign (celu2,'celulares2.txt');
+	menu (arc_log,carga,celu2,nombre);
 		while (loop) do begin
+		writeln ('');
 		write ('SI INGRESA CUALQUIER CARACTER SE DESPLEGARA NUEVAMENTE EL MENU. SI INGRESA E SE CERRARA LA CONSOLA: '); readln (letra);
 		if (letra = 'E') or (letra = 'e') then
 			loop:= false
 		else begin
 			clrscr;
-				menu (arc_log,carga,nombre);
+				menu (arc_log,carga,celu2,nombre);
 		end;
 	end;
 
